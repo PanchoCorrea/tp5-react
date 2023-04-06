42fd09aed32f784bdb71b527987f5907b504948a
PanchoCorrea <correafrancisco2003@gmail.com>
2023-04-05 22:54:19 -0300

    se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from 'react';
+
+function Calculo(){
+
+const[calculo, setCalculo] =useState(0);
+
+const aumentarContador = () => {
+
+  setCalculo(calculo + 1);
 }
 
-export default App;
+const disminuirContador = () => {
+
+  setCalculo(calculo - 1);
+};
+
+
+return(
+
+<div>
+<h1>Presione: {calculo}</h1>
+<button onClick={aumentarContador}>SUMAR</button>
+<button onClick={disminuirContador}>RESTAR</button>
+</div>
+
+);
+
+}
+export default Calculo;
